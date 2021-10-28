85aaac08d88e52406b0422a46deb7227ab185ace
Matthew Underwood <matthewunderwood7@gmail.com>
2021-10-28 15:35:19 -0500

    added four_of_a_kind_check

--- .idea/yup.py
+++ .idea/yup.py
@@ -1,6 +1,6 @@
 import random
 
-dice = [0,0,0,0,0]
+dice = [1,2,1,1,1]
 dicecount = [0,0,0,0,0]
 
 def full_house_check(dice):
@@ -9,18 +9,25 @@ def full_house_check(dice):
     else:
         return "false"
 
-def yahtzee_check(dice):
+def yahtzee_check(dicecount):
     if 5 in dicecount:
         return "true"
     else:
         return "false"
 
-for idx, val in enumerate(dice):
-    dice[idx] = random.randrange(1,6)
-    print (dice)
+def four_of_a_kind_check(dicecount):
+    if 4 in dicecount:
+        return "true"
+    else:
+        return "false"
+
+
+# for idx, val in enumerate(dice):
+#     dice[idx] = random.randrange(1,6)
+#     print (dice)
 
 for idx, val in enumerate(dice):
     dicecount[idx] = dice.count(idx + 1)
 
 
-print (yahtzee_check(dice))
+print (four_of_a_kind_check(dicecount))
