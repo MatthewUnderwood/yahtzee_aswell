222ca31c67ec109ea471269c55c8238907190f48
matthew underwood <matthewunderwood7@gmail.com>
2021-12-27 14:30:08 -0600

    added an excel file with 10,000 games played with high low and average.

--- .idea/yup.py
+++ .idea/yup.py
@@ -26,51 +26,51 @@ pos_of_twos = []
 # main program starts
 ########################################################################
 ########################################################################
-# for onethou_rolls in range(500):
-#     scorecard = [-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1]
-#     extra_yahtzees = [0]
-for thirteen_rolls in range(13):
-    for idx, val in enumerate(dice):
-        dice[idx] = random.randrange(1,7)
-    dice.sort()
-    print ("this is the inital roll")
-    print (dice)
-    position_functions.count_dice(dicecount, dice)
-    for i in range(2):
-        ########################################################################
-        ########################################################################
-        # this commented out part lets the user acctually pick the dice to roll
-        # instead of letting the algo reroll for you
-        ########################################################################
-        ########################################################################
-        # user_amount_of_rerolls = int(input("how many dice would you like to reroll"))
-        # for  i in range(user_amount_of_rerolls):
-        #     user_input = int(input("which dice would you like to reroll"))
-        #     reroll_dice(user_input - 1, dice)
-        # print (dice)
-        ########################################################################
-        ########################################################################
-        matthews_algo.matthews_rolling_algo(dicecount, dice, scorecard, extra_yahtzees, pos_of_ones, pos_of_twos)
+for onethou_rolls in range(500):
+    scorecard = [-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1]
+    extra_yahtzees = [0]
+    for thirteen_rolls in range(13):
+        for idx, val in enumerate(dice):
+            dice[idx] = random.randrange(1,7)
         dice.sort()
-        print(dice)
+        print ("this is the inital roll")
+        print (dice)
         position_functions.count_dice(dicecount, dice)
-    print (matthews_algo.matthews_score_algo(dicecount, dice, scorecard, extra_yahtzees, pos_of_ones, pos_of_twos))
-    print (scorecard)
+        for i in range(2):
+            ########################################################################
+            ########################################################################
+            # this commented out part lets the user acctually pick the dice to roll
+            # instead of letting the algo reroll for you
+            ########################################################################
+            ########################################################################
+            # user_amount_of_rerolls = int(input("how many dice would you like to reroll"))
+            # for  i in range(user_amount_of_rerolls):
+            #     user_input = int(input("which dice would you like to reroll"))
+            #     reroll_dice(user_input - 1, dice)
+            # print (dice)
+            ########################################################################
+            ########################################################################
+            matthews_algo.matthews_rolling_algo(dicecount, dice, scorecard, extra_yahtzees, pos_of_ones, pos_of_twos)
+            dice.sort()
+            print(dice)
+            position_functions.count_dice(dicecount, dice)
+        print (matthews_algo.matthews_score_algo(dicecount, dice, scorecard, extra_yahtzees, pos_of_ones, pos_of_twos))
+        print (scorecard)
 
-score_total = 0
-short_sum = 0
-for i in range(6):
-    short_sum = short_sum + scorecard[i]
-if short_sum > 62:
-    score_total = score_total + 35
-if scorecard[11] != 0:
-    score_total = score_total + sum(scorecard) + (extra_yahtzees[0] * 100)
-else:
-    score_total = score_total + sum(scorecard)
-print("your total score is " + str(score_total))
-print(extra_yahtzees[0])
+    score_total = 0
+    short_sum = 0
+    for i in range(6):
+        short_sum = short_sum + scorecard[i]
+    if short_sum > 62:
+        score_total = score_total + 35
+    if scorecard[11] != 0:
+        score_total = score_total + sum(scorecard) + (extra_yahtzees[0] * 100)
+    else:
+        score_total = score_total + sum(scorecard)
+    print("your total score is " + str(score_total))
+    print(extra_yahtzees[0])
 
     # this saves the score to an out txt file
-    # with open('out.txt', 'a') as f:
-    #     with redirect_stdout(f):
-    #         print(str(score_total))
+    with open('out.txt', 'a') as f:
+        with redirect_stdout(f):
+            print(str(score_total))
